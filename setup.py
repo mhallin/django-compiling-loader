02f3ff648da25f4d44031bc62a5e45d6a5b15c90
mhallin <mhallin@gmail.com>
2015-07-20 12:44:49 +0200

    Add missing parts of Python package structure

--- setup.py
+++ setup.py
@@ -5,6 +5,15 @@ setup(
     version='0.0.1',
     packages=find_packages(exclude=['test_proj']),
 
+    url='https://github.com/mhallin/django-compiling-loader',
+    description='A bytecode compiling template loader for Django',
+    long_description=open('README.rst').read(),
+
+    author='Magnus Hallin',
+    author_email='mhallin@gmail.com',
+
+    license='BSD',
+
     extras_require={
         'test': [
             'pytest~=2.7.2',
@@ -12,5 +21,27 @@ setup(
             'pytest-django~=2.8.0',
             'Django~=1.7.0'
         ],
+        'dist': [
+            'twine',
+            'wheel'
+        ]
     },
+
+    classifiers=[
+        'Development Status :: 4 - Beta',
+
+        'Framework :: Django',
+        'Framework :: Django :: 1.7',
+
+        'Intended Audience :: Developers',
+
+        'License :: OSI Approved :: BSD License',
+
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+
+        'Topic :: Software Development :: Compilers',
+    ],
+
+    keywords='django performance template',
 )
